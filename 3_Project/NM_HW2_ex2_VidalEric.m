c5ffe3f0e6c7938299650db4319afecab1de8e88
Eric Vidal <ericvidalmarcos@gmail.com>
2024-02-05 02:08:56 +0100

    just one exercise to finish

--- 3_Project/NM_HW2_ex2_VidalEric.m
+++ 3_Project/NM_HW2_ex2_VidalEric.m
@@ -34,6 +34,16 @@ x_pos = (xmax + x_step/2):x_step:(100 - x_step/2); % Å, Discretized grid for x
 % Total grid
 xx = [x_neg, x_U, x_pos]; % Å, Discretized grid for x
 
+% Energy discretization
+E_step = 0.0005  % eV, Step for the energy
+Emin = 0.0       % eV, Minimum energy
+Emax = 0.3       % eV, Maximum energy
+EE = (Emin + E_step/2):E_step:(Emax - E_step/2); % eV, Discretized energy
+% E length
+numE= length(EE);
+% Recombination time (lifetime) parameter and damping factor
+recombT = 1.0E-9; % ns
+damping=(hbar * 2*pi / recombT) / qel;
 
 % Potential U(x) for the quantum junction
 Ux = BarrierPotential(x_U, 0, 15, 0.2) + BarrierPotential(x_U, 65, 80, 0.2);
@@ -64,10 +74,7 @@ set(gcf, 'Color', [0.7 0.7 0.7]);
 % set(gca,'TickLength',[0.03, 0.02]);
 
 
-%% LOCALIZED GREEN'S FUNCTION METHOD
-% The Green's function for the Schrodinger equation is given by
-% (eq. A.76) and (eq. A.77) in the book of NM
-
+%% PERTURBATION
 % According to equation (4.13) for solving the step perturbation with
 % V' at the end is what we had in the total potential - 0th order solution:
 Uperturbation = Ubiased - Ebias*ones(1,length(x_U));
@@ -92,6 +99,87 @@ set(gca, 'Color', [0.9 0.9 0.9], 'GridColor', [0.5 0.5 0.5]);
 % Set the background color of the figure to a darker gray
 set(gcf, 'Color', [0.7 0.7 0.7]);
 
+%% WAVE FUNCTIONS
+% It comes from the dispersion relations binding E to the wavevector
+% is not conserved across the step. Leading to eq. 58 in the book of NM
+commonTerm = 2*elm*qel/hbar^2;
+% for x<0
+k0min = real(sqrt((Emin + 1i*damping) * commonTerm));
+wavelenght0min = 2*pi*(1E+10)/k0min % Å, Shortest Wavelength
+k0max = real(sqrt((Emax + 1i*damping) * commonTerm));
+wavelenght0max = 2*pi*(1E+10)/k0max % Å, Longest Wavelength
+% for x>0
+k1min = real(sqrt((Emin - Ebias + 1i*damping) * commonTerm));
+wavelenght1min = 2*pi*(1E+10)/k1min % Å, Shortest Wavelength
+k1max = real(sqrt((Emax - Ebias + 1i*damping) * commonTerm));
+wavelenght1max = 2*pi*(1E+10)/k1max % Å, Longest Wavelength
 
+
+%% LOCALIZED GREEN'S FUNCTION METHOD
+% The Green's function for the Schrodinger equation is given by
+% (eq. A.76) and (eq. A.77) in the book of NM
+
+% Production of R(E), T(E) and A(E) curves for the biased quantum junction
+% Same conditions as in Exercise 1, except step which is the bias now.
+for i=1:numE
+    [RR(i), TT(i), AA(i)] = RTA(Ebias, EE(i), damping, x_U, Uperturbation, x_step, ekinscale);
+end
+
+% Plot of R(E), T(E) and A(E)
+figure;
+plot(EE, RR, 'LineWidth', 2);
+hold on;
+plot(EE, TT, 'LineWidth', 2);
+plot(EE, AA, 'LineWidth', 2);
+title('R(E), T(E) and A(E) for the Biased Quantum Junction', 'fontsize', 26);
+xlabel('E (eV)','FontSize',18);
+ylabel('R(E), T(E), A(E)','FontSize',18);
+ylim([0,1]);
+set(gca,'Box','on');
+set(gca,'linewidth',1);
+grid on;
+% Set the color of the axes and the grid to a light gray
+set(gca, 'Color', [0.9 0.9 0.9], 'GridColor', [0.5 0.5 0.5]);
+% Set the background color of the figure to a darker gray
+set(gcf, 'Color', [0.7 0.7 0.7]);
+% set(gca,'TickLength',[0.03, 0.02]);
+legend('R(E)', 'T(E)', 'A(E)', 'Location', 'Best');
+
+
+%% ELECTRON AT THE FERMI LEVEL
+% Set E0=0.01eV and explore the effect of tuning the bias
+% U1 in [-0.2, 0.2]eV
+U0min = -0.2; % eV
+U0max = 0.2; % eV
+
+for i=1:length(x_U)
+    Ubiased(i) = Ux(i) + U0min*x_U(i)/(xmax-xmin);
+end
+U0min_biased = [zeros(1,length(x_neg)), Ubiased, U0min*ones(1,length(x_pos))];
+
+for i=1:length(x_U)
+    Ubiased(i) = Ux(i) + U0max*x_U(i)/(xmax-xmin);
+end
+U0max_biased = [zeros(1,length(x_neg)), Ubiased, U0max*ones(1,length(x_pos))];
+% Plot the biased potential U(x)
+figure(4)
+plot(xx, Ufull, 'b', xx, U0min_biased, 'r', xx, U0max_biased, 'g')
+title('Potential U(x) for the quantum junction')
+xlabel('x [Å]')
+ylabel('U(x) [eV]')
+set(gca,'Box','on');
+set(gca,'linewidth',1);
+legend('U(x)', 'U(x) + U_{biased}(x) [U_{1}(x)=-0.2eV]', 'U_{biased}(x) [U_{1}(x)=0.2eV]')
+grid on
+% Set the color of the axes and the grid to a light gray
+set(gca, 'Color', [0.9 0.9 0.9], 'GridColor', [0.5 0.5 0.5]);
+% Set the background color of the figure to a darker gray
+set(gcf, 'Color', [0.7 0.7 0.7]);
+% set(gca,'TickLength',[0.03, 0.02]);
+
+
+%% RTA FOR THE ELECTRON AT THE FERMI LEVEL
+% Fine U1 step of 0.0005eV, compute current-bias curve in this range of bias
+% Electrical resistance, diode?
 
 toc
